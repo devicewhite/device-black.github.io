c68eec0a1c8c24c1ab8fae44edbdc6f7d7e54195
DeviceBlack <65994174+Device-Black@users.noreply.github.com>
2023-04-18 20:13:34 -0300

    update copyright.js

--- copyright.js
+++ copyright.js
@@ -1,30 +1,30 @@
-const elements = document.getElementsByTagName('js-item')
+const HighElements = document.getElementsByTagName('js-item')
 
-for(let i = 0; i < elements.length; i++)
+for(let i = 0; i < HighElements.length; i++)
 {
-	let legend = elements[i].innerHTML
-	elements[i].innerHTML = `<div class="box-content"><div class="box-square"></div><b class="box-legend">${legend}</b></div>`;
+	let legend = HighElements[i].innerHTML
+	HighElements[i].innerHTML = `<div class="box-content"><div class="box-square"></div><b class="box-legend">${legend}</b></div>`;
 
-	if(!elements[i].hasAttribute('checked')) elements[i].setAttribute('checked', 'false');
-	if(!elements[i].hasAttribute('value')) elements[i].setAttribute('value', '0');
+	if(!HighElements[i].hasAttribute('checked')) HighElements[i].setAttribute('checked', 'false');
+	if(!HighElements[i].hasAttribute('value')) HighElements[i].setAttribute('value', '0');
 
-	let boxContent = elements[i].querySelector('.box-content')
+	let boxContent = HighElements[i].querySelector('.box-content')
 	boxContent.setAttribute('onclick', `itemCheck(${i})`);
 	boxContent.style.display = 'flex';
 	boxContent.style.padding = '2px';
 	boxContent.style.margin = '2px';
 	
-	let boxSquare = elements[i].querySelector('.box-square')
+	let boxSquare = HighElements[i].querySelector('.box-square')
 	boxSquare.style.width = '10px';
 	boxSquare.style.height = '10px';
 	boxSquare.style.border = '2px solid black';
-	if(elements[i].getAttribute('checked') == 'true') {
+	if(HighElements[i].getAttribute('checked') == 'true') {
 		boxSquare.style.backgroundColor = '#00AAFF';
 	} else {
 		boxSquare.style.backgroundColor = 'gray';
 	}
 	
-	let boxLegend = elements[i].querySelector('.box-legend')
+	let boxLegend = HighElements[i].querySelector('.box-legend')
 	boxLegend.style.fontSize = '12px';
 	boxLegend.style.marginLeft = '2px';
 }
