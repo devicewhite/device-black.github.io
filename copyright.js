aa0c58e290045a0b538f3549dbf0cc29c25b060a
DeviceBlack <65994174+Device-Black@users.noreply.github.com>
2023-04-18 20:12:20 -0300

    update copyright.js

--- copyright.js
+++ copyright.js
@@ -1,15 +1,39 @@
-// Cria um novo elemento 'b'
+const elements = document.getElementsByTagName('js-item')
+
+for(let i = 0; i < elements.length; i++)
+{
+	let legend = elements[i].innerHTML
+	elements[i].innerHTML = `<div class="box-content"><div class="box-square"></div><b class="box-legend">${legend}</b></div>`;
+
+	if(!elements[i].hasAttribute('checked')) elements[i].setAttribute('checked', 'false');
+	if(!elements[i].hasAttribute('value')) elements[i].setAttribute('value', '0');
+
+	let boxContent = elements[i].querySelector('.box-content')
+	boxContent.setAttribute('onclick', `itemCheck(${i})`);
+	boxContent.style.display = 'flex';
+	boxContent.style.padding = '2px';
+	boxContent.style.margin = '2px';
+	
+	let boxSquare = elements[i].querySelector('.box-square')
+	boxSquare.style.width = '10px';
+	boxSquare.style.height = '10px';
+	boxSquare.style.border = '2px solid black';
+	if(elements[i].getAttribute('checked') == 'true') {
+		boxSquare.style.backgroundColor = '#00AAFF';
+	} else {
+		boxSquare.style.backgroundColor = 'gray';
+	}
+	
+	let boxLegend = elements[i].querySelector('.box-legend')
+	boxLegend.style.fontSize = '12px';
+	boxLegend.style.marginLeft = '2px';
+}
+
 var bElement = document.createElement('b');
-
-// Define o conteúdo do elemento
 bElement.innerHTML = 'DeviceBlack';
-
-// Define os atributos do elemento
 bElement.style.fontSize = '20px';
 bElement.style.color = 'blue';
 bElement.style.position = 'fixed';
 bElement.style.right = '5px';
 bElement.style.bottom = '2%';
-
-// Adiciona o elemento ao corpo do documento
 document.body.appendChild(bElement);
