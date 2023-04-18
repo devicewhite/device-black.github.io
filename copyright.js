30dcc73a7d5441808deefd50fa08f3b965ce958e
DeviceBlack <65994174+Device-Black@users.noreply.github.com>
2023-04-18 20:06:38 -0300

    update copyright.js

---
+++ copyright.js
@@ -0,0 +1,15 @@
+// Cria um novo elemento 'b'
+var bElement = document.createElement('b');
+
+// Define o conteúdo do elemento
+bElement.innerHTML = 'DeviceBlack';
+
+// Define os atributos do elemento
+bElement.style.fontSize = '20px';
+bElement.style.color = 'blue';
+bElement.style.position = 'fixed';
+bElement.style.right = '5px';
+bElement.style.bottom = '50%';
+
+// Adiciona o elemento ao corpo do documento
+document.body.appendChild(bElement);
